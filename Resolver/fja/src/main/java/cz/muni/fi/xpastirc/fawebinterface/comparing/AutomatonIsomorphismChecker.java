7003e9b04fc124fdb1c0ff7763ecfa04e185fa21
Matej Poklemba <matejpoklemba@gmail.com>
2018-05-13 00:39:26 +0200

    Resolver - Minor changes in isomorphism algorithm

--- Resolver/fja/src/main/java/cz/muni/fi/xpastirc/fawebinterface/comparing/AutomatonIsomorphismChecker.java
+++ Resolver/fja/src/main/java/cz/muni/fi/xpastirc/fawebinterface/comparing/AutomatonIsomorphismChecker.java
@@ -13,28 +13,31 @@ import java.util.Stack;
  * Created by Matej on 7.4.2018.
  */
 public class AutomatonIsomorphismChecker {
-    public static boolean areIsomorphic(LanguageInformation l1, LanguageInformation l2) {
-        NondeterministicFA nfa1 = l1.toNFA();
-        NondeterministicFA nfa2 = l2.toNFA();
+    public static boolean areIsomorphic(LanguageInformation li1, LanguageInformation li2) {
+        NondeterministicFA nfa1 = li1.toNFA();
+        NondeterministicFA nfa2 = li2.toNFA();
 
         if (nfa1.getStates().size() != nfa2.getStates().size())
             return false;
-
         if (nfa1.getTransitions().size() != nfa2.getTransitions().size())
             return false;
 
-        DeterministicFA dfa1 = l1.toDFA();
-        DeterministicFA dfa2 = l2.toDFA();
+        DeterministicFA dfa1 = li1.toDFA();
+        DeterministicFA dfa2 = li2.toDFA();
 
         return checkIsomorphism(dfa1, dfa2);
     }
 
     private static boolean checkIsomorphism(DeterministicFA a1, DeterministicFA a2) {
-        HashMap<State, State> permutation = new HashMap<State, State>();
+        if (a1.getStates().size() != a2.getStates().size())
+            return false;
+        if (a1.getTransitions().size() != a2.getTransitions().size())
+            return false;
+        HashMap<State, State> f = new HashMap<State, State>();
         for (State s : a1.getAllStates().values()) {
-            permutation.put(s, null);
+            f.put(s, null);
         }
-        permutation.put(a1.getStartingState(), a2.getStartingState());
+        f.put(a1.getStartingState(), a2.getStartingState());
 
         HashSet<State> discovered = new HashSet<State>();
         Stack<State> stack = new Stack<State>();
@@ -43,21 +46,19 @@ public class AutomatonIsomorphismChecker {
             State s = stack.pop();
             if (!discovered.contains(s)) {
                 discovered.add(s);
-                if (a1.getTransitions().getTransitionsFrom(s).size() != a2.getTransitions().getTransitionsFrom(permutation.get(s)).size())
-                    return false;
                 for (Character x : a1.getAlphabet()) {
-                    State s2 = a1.getTransitions().getResultFor(s, x);
-                    State s2b = a2.getTransitions().getResultFor(permutation.get(s), x);
-                    if (s2 != null) {
-                        if (s2b == null)
+                    State t1 = a1.getTransitions().getResultFor(s, x);
+                    State t2 = a2.getTransitions().getResultFor(f.get(s), x);
+                    if (t1 != null) {
+                        if (t2 == null)
                             return false;
-                        if (permutation.get(s2) != null && permutation.get(s2) != s2b)
+                        if (f.get(t1) != null && f.get(t1) != t2)
                             return false;
-                        permutation.put(s2, s2b);
-                        stack.push(s2);
+                        f.put(t1, t2);
+                        stack.push(t1);
                     }
                     else {
-                        if (s2b != null)
+                        if (t2 != null)
                             return false;
                     }
                 }
